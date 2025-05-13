552f552830b17a8512b2a6e9188055b950d302d9
yeeeww <yeangsshi@ewhain.net>
2025-05-13 13:42:37 +0900

    Remove: 250508 폴더 삭제

--- JavaScript_3/array/array.js
+++ JavaScript_3/array/array.js
@@ -1,25 +1,25 @@
 const lionsA = [
-  { name: "김수민", track: "기획디자인", deptNum: 98 },
-  { name: "김지후", track: "기획디자인", deptNum: 44 },
-  { name: "박서영", track: "기획디자인", deptNum: 31 },
-  { name: "송나영", track: "기획디자인", deptNum: 22 },
-  { name: "이도윤", track: "기획디자인", deptNum: 85 },
-  { name: "이은서", track: "기획디자인", deptNum: 44 },
-  { name: "김민혜", track: "프론트엔드", deptNum: 76 },
-  { name: "김성주", track: "프론트엔드", deptNum: 71 },
-  { name: "서예린", track: "프론트엔드", deptNum: 66 },
-  { name: "유진서", track: "프론트엔드", deptNum: 76 },
-  { name: "이예영", track: "프론트엔드", deptNum: 46 },
-  { name: "최연서", track: "프론트엔드", deptNum: 23 },
+	{ name: "김수민", track: "기획디자인", deptNum: 98 },
+	{ name: "김지후", track: "기획디자인", deptNum: 44 },
+	{ name: "박서영", track: "기획디자인", deptNum: 31 },
+	{ name: "송나영", track: "기획디자인", deptNum: 22 },
+	{ name: "이도윤", track: "기획디자인", deptNum: 85 },
+	{ name: "이은서", track: "기획디자인", deptNum: 44 },
+	{ name: "김민혜", track: "프론트엔드", deptNum: 76 },
+	{ name: "김성주", track: "프론트엔드", deptNum: 71 },
+	{ name: "서예린", track: "프론트엔드", deptNum: 66 },
+	{ name: "유진서", track: "프론트엔드", deptNum: 76 },
+	{ name: "이예영", track: "프론트엔드", deptNum: 46 },
+	{ name: "최연서", track: "프론트엔드", deptNum: 23 },
 ];
 
 const lionsB = [
-  { name: "김가영", track: "백엔드", deptNum: 71 },
-  { name: "김연우", track: "백엔드", deptNum: 73 },
-  { name: "성혜린", track: "백엔드", deptNum: 71 },
-  { name: "설영은", track: "백엔드", deptNum: 48 },
-  { name: "신지민", track: "백엔드", deptNum: 66 },
-  { name: "황규리", track: "백엔드", deptNum: 76 },
+	{ name: "김가영", track: "백엔드", deptNum: 71 },
+	{ name: "김연우", track: "백엔드", deptNum: 73 },
+	{ name: "성혜린", track: "백엔드", deptNum: 71 },
+	{ name: "설영은", track: "백엔드", deptNum: 48 },
+	{ name: "신지민", track: "백엔드", deptNum: 66 },
+	{ name: "황규리", track: "백엔드", deptNum: 76 },
 ];
 
 //문제 1) lions 배열에 lionsB 배열을 합치기 (스프레드 사용)
